f06d6d4cd5425485ed9798a2e31029aac8e5aa0c
Maximiliano Paz <maximilianopaz@Maximilianos-MacBook-Pro.local>
2023-12-17 21:07:26 -0300

    add ref to home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,15 +3,18 @@ import styles from "./Header.module.css";
 import Logo from "../Logo";
 import UserNavbar from "../UserNavbar";
 import SiteNavbar from "../SiteNavbar";
+import Link from "next/link";
 
 function Header() {
   return (
     <header className={styles.wrapper}>
       <SiteNavbar />
       <div className="h-[67px] flex ">
-        <h1 className="m-auto text-center ml-[233px]">
-          Tapiceria Nautica Buenos Aires
-        </h1>
+        <Link href="/" className="m-auto  ">
+          <h1 className="m-auto text-center ml-[233px]">
+            Tapiceria Nautica Buenos Aires
+          </h1>
+        </Link>
       </div>
       <UserNavbar />
     </header>
